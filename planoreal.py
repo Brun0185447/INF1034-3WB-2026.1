726fe4ba8ed4021d2e616cacec5bd9fe84783c3c
Brun0185447 <brunosantiagooliveira2007@gmail.com>
2026-04-01 20:29:16 -0300

    Update plotting functions and add delays
    Refactor plotting functions and add sleep and clear calls.

--- planoreal.py
+++ planoreal.py
@@ -26,6 +26,22 @@ t.goto(0, raiz_u(0))
 t.pd()
 for x in range(1, 100):
     t.goto(2*x, 10*raiz_u(2*x))
+sleep(3)
+t.clear()
+
+t.color('black')
+t.pu()
+t.goto(-300, 0)
+t.pd()
+t.goto(300, 0)
+t.stamp()
+
+t.pu()
+t.goto(0, -300)
+t.pd()
+t.goto(0, 300)
+t.lt(90)
+t.stamp()
 
 def funcao_a(x):
     return 1/x
@@ -41,36 +57,107 @@ t.goto(-100, 100*funcao_a(-100))
 t.pd()
 for x in range(-99, -1):
     t.goto(2*x, 100*funcao_a(x))
+sleep(3)
+t.clear()
+
+t.color('black')
+t.pu()
+t.goto(-300, 0)
+t.pd()
+t.goto(300, 0)
+t.stamp()
+
+t.pu()
+t.goto(0, -300)
+t.pd()
+t.goto(0, 300)
+t.lt(90)
+t.stamp()
 
 t.color('green')
 def funcao_b(x):
     return 2**x
 t.pu()
-t.goto(2, 100*funcao_b(2))
+t.goto(-5, funcao_b(-5))
 t.pd()
-for x in range(3, 8):
-    t.goto(x, 100*funcao_b(x))
-for x in range(-99, -2):
-    t.goto(x, 100*funcao_b(x))
+for x in range(-97, -1):
+    t.goto(x, 450*funcao_b(x))
+sleep(3)
+t.clear()
 
+t.color('black')
+t.pu()
+t.goto(-300, 0)
+t.pd()
+t.goto(300, 0)
+t.stamp()
 
+t.pu()
+t.goto(0, -300)
+t.pd()
+t.goto(0, 300)
+t.lt(90)
+t.stamp()
 
 t.color('yellow')
 def funcao_d(x):
     return 5 - x**2
-
+t.pu()
+t.goto(7, funcao_d(7))
+t.pd()
+for x in range(1, 12):
+    t.goto(x, funcao_d(x))
+sleep(3)
+t.clear()
 
 t.color('black')
+t.pu()
+t.goto(-300, 0)
+t.pd()
+t.goto(300, 0)
+t.stamp()
+
+t.pu()
+t.goto(0, -300)
+t.pd()
+t.goto(0, 300)
+t.lt(90)
+t.stamp()
+
+t.color('purple')
 def funcao_e(x):
     return x**2 - 5*x + 6
+t.pu()
+t.goto(1, funcao_e(1))
+t.pd()
+for x in range(1, 20):
+    t.goto(x, funcao_e(x))
+sleep(3)
+t.clear()
 
+t.color('black')
+t.pu()
+t.goto(-300, 0)
+t.pd()
+t.goto(300, 0)
+t.stamp()
+
+t.pu()
+t.goto(0, -300)
+t.pd()
+t.goto(0, 300)
+t.lt(90)
+t.stamp()
 
 t.color('orange')
 def funcao_f(x):
     return x**3 - x**2 -x + 1
-
-
-
-
+t.pu()
+t.goto(-1, funcao_f(-1))
+t.pd()
+for x in range(-1, 20):
+    t.goto(x, funcao_e(x))
+sleep(3)
+t.clear()
 
 mainloop()
