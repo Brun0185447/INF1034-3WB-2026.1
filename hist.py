4f56eada42bf20de6756c913ee584e39406f7d3c
Brun0185447 <brunosantiagooliveira2007@gmail.com>
2026-05-25 12:41:45 -0300

    Update hist.py

--- hist.py
+++ hist.py
@@ -3,7 +3,7 @@ import random
 import sys
 
 pygame.init()
-tela = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Visualizador Interativo de Histogramas")
 fonte = pygame.font.SysFont(None, 24)
 
