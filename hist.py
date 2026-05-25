58908bd14351b195f6aeab29233a41b1ecd3cafa
Bruno Santiago de Oliveira <brunosantiagooliveira2007@gmail.com>
2026-05-25 12:23:42 -0300

    tr

--- hist.py
+++ hist.py
@@ -3,7 +3,7 @@ import random
 import sys
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+tela = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Visualizador Interativo de Histogramas")
 fonte = pygame.font.SysFont(None, 24)
 
